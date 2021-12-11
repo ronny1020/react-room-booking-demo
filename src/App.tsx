692782e9e811ab90ad3ab13ad24def2e7830bd8b
Ronny Hsieh <46809311+ronny1020@users.noreply.github.com>
2021-12-11 21:56:30 +0800

    style(eslint): setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from "react";
+import React from 'react'
 
+const App: React.FC = function App() {
+  return <h1> Hello World </h1>
+}
 
-const App: React.FC<{}> = () => {
-  return (
-    <>
-        <h1> Hello World </h1>
-    </>
-  );
-};
-
-export default App;
+export default App
